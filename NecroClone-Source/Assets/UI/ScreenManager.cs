23c7f3ae009fa12e7f6d1a7f58cd296b1e1dd343
TBartl <tbartl@umich.edu>
2017-10-20 15:15:12 -0400

    Added screen manager

---
+++ NecroClone-Source/Assets/UI/ScreenManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour {
+
+    public static ScreenManager S;
+
+    GameObject currentScreen;
+
+    public GameObject titleScreen;
+    public GameObject lobbyScreen;
+
+    void Awake() {
+        S = this;   
+    }
+
+    void Start() {
+		currentScreen = (GameObject)Instantiate(titleScreen);
+    }
+
+}
