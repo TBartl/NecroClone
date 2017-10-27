c6907c07f053a0e128e00927ee0bffd0b52cd3a1
TBartl <tbartl@umich.edu>
2017-10-27 13:05:08 -0400

    Longswords and broadswords

---
+++ NecroClone-Source/Assets/Occupants/Actions/Hit/HitPatternBroadsword.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "HitPatterns/Broadsword")]
+public class HitPatternBroadsword : HitPattern {
+
+	public override List<GameObject> GetValidTargets(GameObject source, IntVector2 direction) {
+		List<GameObject> validTargets = new List<GameObject>();
+		IntVector2 targetPos = direction + source.GetComponent<IntTransform>().GetPos();
+
+		// TODO: Handle diagonal here?
+		AddIfValidTarget(ref validTargets, source, targetPos);
+
+		if (direction.x != 0) {
+			AddIfValidTarget(ref validTargets, source, targetPos + IntVector2.up);
+			AddIfValidTarget(ref validTargets, source, targetPos + IntVector2.down);
+		} else {
+			AddIfValidTarget(ref validTargets, source, targetPos + IntVector2.left);
+			AddIfValidTarget(ref validTargets, source, targetPos + IntVector2.right);
+		}
+
+		return validTargets;
+	}
+}
