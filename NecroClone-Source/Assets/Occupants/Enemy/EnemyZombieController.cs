3f315bfadd607b96b8e5765ae00ec954df6c9f52
TBartl <tbartl@umich.edu>
2017-10-29 02:17:00 -0400

    Zombies

---
+++ NecroClone-Source/Assets/Occupants/Enemy/EnemyZombieController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyZombieController : Controller {
+
+	public bool horizontal = true;
+	bool to;
+
+	ActionAutoHitDigOrMove hitDigOrMove;
+
+	protected override void Awake() {
+		base.Awake();
+		hitDigOrMove = new ActionAutoHitDigOrMove(this.gameObject);
+	}
+
+	protected override void OnRecoverFinished() {
+		IntVector2 direction = IntVector2.zero;
+		if (horizontal)
+			direction = to ? IntVector2.right : IntVector2.left;
+		else
+			direction = to ? IntVector2.up : IntVector2.down;
+		IntVector2 start = intTransform.GetPos();
+		DoAction(hitDigOrMove.GetAction(direction), direction);
+		IntVector2 end = intTransform.GetPos();
+
+		if (start == end)
+			to = !to;
+	}
+}
